26ce5be9b383e9112057a637823f6fb4160df93c
swedishsailor <kzygowski7@gmail.com>
2022-02-19 01:12:30 +0100

    typescript react init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Hero from './components/Hero/Hero';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero></Hero>
+    </div>
+  );
+}
+
+export default App;
